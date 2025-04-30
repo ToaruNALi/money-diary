c80f0aa540c6399648961277902d0c16dfcf04f0
NALi <narisan3050@gmail.com>
2025-04-30 14:33:10 +0900

    グラフ表示見直し1

--- src/app/shared/chart/dialog-chart/dialog-chart.component.ts
+++ src/app/shared/chart/dialog-chart/dialog-chart.component.ts
@@ -1,14 +1,18 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
   Inject,
   signal,
 } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AgChartsModule } from 'ag-charts-angular';
 import { AgChartOptions } from 'ag-charts-community';
+import * as DateUtil from 'date-fns';
 import { RowData } from 'src/app/domain/row-data';
 import * as Const from 'src/app/shared/constants/constants';
+import * as Util from 'src/app/shared/constants/utils';
+import { DialogCommonModule } from 'src/app/shared/dialog-common.module';
 import { SharedCommonModule } from 'src/app/shared/shared-common.module';
 
 export type DialogChart = {
@@ -17,12 +21,14 @@ export type DialogChart = {
 };
 
 @Component({
-    imports: [SharedCommonModule, AgChartsModule],
-    templateUrl: './dialog-chart.component.html',
-    styleUrl: './dialog-chart.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  imports: [SharedCommonModule, DialogCommonModule, AgChartsModule],
+  templateUrl: './dialog-chart.component.html',
+  styleUrl: './dialog-chart.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DialogChartComponent {
+  protected readonly dialogRef = inject(MatDialogRef);
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
     private readonly data: DialogChart,
@@ -34,97 +40,105 @@ export class DialogChartComponent {
   private readonly date = signal('date');
 
   ngOnInit(): void {
+    const chartDatas: RowData[] = [];
+    const startDate: string = Const.MANAGEMENT_DATE.START;
+    const endDate = new Date();
+    const xKeys = [Const.SUMMARY_COL_ID.DATE];
+    const yKeys = [Const.SUMMARY_COL_ID.INCOME, Const.SUMMARY_COL_ID.EXPENSES];
+
+    for (
+      let date = startDate;
+      DateUtil.differenceInCalendarMonths(endDate, date) >= 0;
+      date = DateUtil.format(
+        DateUtil.addMonths(date, 1),
+        Const.DATE_FORMAT.YYYY_MM_DD,
+      )
+    ) {
+      const chartData: RowData = {};
+      for (const key of xKeys) {
+        chartData[key] = DateUtil.format(date, Const.DATE_FORMAT.YYYY_MM);
+      }
+      for (const key of yKeys) {
+        chartData[key] = Util.getInitValue(Const.ROW_DATA_KEY.SUMMARY, key);
+      }
+      chartDatas.push(chartData);
+    }
+
+    for (const rowData of this.data.inputDatas) {
+      // chartDatas.push(data);
+      if (!Util.checkInputMode(rowData, Const.INPUT_MODE.ALL_REQ)) {
+        // 必須項目漏れあり
+        continue;
+      }
+
+      const dateStr = rowData[Const.MONEY_DIARY_COL_ID.DATE]?.toString() ?? '';
+      const startDateStr = DateUtil.format(
+        DateUtil.startOfMonth(dateStr),
+        Const.DATE_FORMAT.YYYY_MM_DD,
+      );
+      const chartData = chartDatas.find(
+        (data) =>
+          DateUtil.differenceInCalendarMonths(
+            data[Const.SUMMARY_COL_ID.DATE]?.toString() ?? '',
+            startDateStr,
+          ) === 0,
+      );
+
+      if (!chartData) {
+        // 集計データ内に該当月行なし
+        continue;
+      }
+
+      const amountNum = Number(rowData[Const.MONEY_DIARY_COL_ID.AMOUNT_NUM]);
+
+      if (amountNum > 0) {
+        (chartData[Const.SUMMARY_COL_ID.INCOME] as number) += amountNum;
+      } else if (amountNum < 0) {
+        (chartData[Const.SUMMARY_COL_ID.EXPENSES] as number) += amountNum;
+      }
+    }
+
     this.options.set({
-      title: {
-        text: "Apple's Revenue by Product Category",
-      },
-      subtitle: {
-        text: 'In Billion U.S. Dollars',
-      },
-      data: this.data.inputDatas,
+      theme: 'ag-default-dark',
+      axes: [
+        {
+          type: 'number',
+          position: 'left',
+          label: {
+            formatter: ({ value }) => {
+              return Util.cvtNumToPrice(value);
+            },
+          },
+          crosshair: {
+            strokeWidth: 100,
+          },
+          interval: {
+            minSpacing: 80,
+            maxSpacing: 120,
+          },
+        },
+        {
+          type: 'category',
+          position: 'bottom',
+          paddingOuter: 1,
+        },
+      ],
+      data: chartDatas,
       series: [
         {
           type: 'bar',
-          xKey: 'dt',
-          yKey: 'an',
-          yName: 'Amount',
+          xKey: Const.SUMMARY_COL_ID.DATE,
+          yKey: Const.SUMMARY_COL_ID.INCOME,
+          yName: 'Income',
+        },
+        {
+          type: 'bar',
+          xKey: Const.SUMMARY_COL_ID.DATE,
+          yKey: Const.SUMMARY_COL_ID.EXPENSES,
+          yName: 'Expenses',
         },
       ],
     });
-    // this.options.set({
-    //   title: {
-    //     text: "Apple's Revenue by Product Category",
-    //   },
-    //   subtitle: {
-    //     text: 'In Billion U.S. Dollars',
-    //   },
-    //   data: [
-    //     {
-    //       quarter: "Q1'18",
-    //       iphone: 140,
-    //       mac: 16,
-    //       ipad: 14,
-    //       wearables: 12,
-    //       services: 20,
-    //     },
-    //     {
-    //       quarter: "Q2'18",
-    //       iphone: 124,
-    //       mac: 20,
-    //       ipad: 14,
-    //       wearables: 12,
-    //       services: 30,
-    //     },
-    //     {
-    //       quarter: "Q3'18",
-    //       iphone: 112,
-    //       mac: 20,
-    //       ipad: 18,
-    //       wearables: 14,
-    //       services: 36,
-    //     },
-    //     {
-    //       quarter: "Q4'18",
-    //       iphone: 118,
-    //       mac: 24,
-    //       ipad: 14,
-    //       wearables: 14,
-    //       services: 36,
-    //     },
-    //   ],
-    //   series: [
-    //     {
-    //       type: 'bar',
-    //       xKey: 'quarter',
-    //       yKey: 'iphone',
-    //       yName: 'iPhone',
-    //     },
-    //     {
-    //       type: 'bar',
-    //       xKey: 'quarter',
-    //       yKey: 'mac',
-    //       yName: 'Mac',
-    //     },
-    //     {
-    //       type: 'bar',
-    //       xKey: 'quarter',
-    //       yKey: 'ipad',
-    //       yName: 'iPad',
-    //     },
-    //     {
-    //       type: 'bar',
-    //       xKey: 'quarter',
-    //       yKey: 'wearables',
-    //       yName: 'Wearables',
-    //     },
-    //     {
-    //       type: 'bar',
-    //       xKey: 'quarter',
-    //       yKey: 'services',
-    //       yName: 'Services',
-    //     },
-    //   ],
-    // });
   }
 
   /**
@@ -145,7 +159,5 @@ export class DialogChartComponent {
     //   this.date() === 'date'? (date: ValueType) => date :
     //   this.date() === 'payDate'? (date: ValueType) => !!date? ;
     // 縦軸の値を設定する
-
-    //
   };
 }
