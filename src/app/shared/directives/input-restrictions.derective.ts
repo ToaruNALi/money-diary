04332b685c73fb35fa98be9ad84cc0c4b6693632
NALi <narisan3050@gmail.com>
2025-12-14 15:15:06 +0900

    共通部品修正コミット漏れ（入力値チェック）

--- src/app/shared/directives/input-restrictions.derective.ts
+++ src/app/shared/directives/input-restrictions.derective.ts
@@ -1,29 +1,28 @@
 import { Directive, HostListener, inject, input } from '@angular/core';
 import { NgControl } from '@angular/forms';
 import * as Const from 'src/app/shared/constants/constants';
+import { InputRestrictions } from 'src/app/shared/constants/types';
 
 @Directive({
   selector: '[appInputRestrictions]',
 })
 export class InputRestrictionsDirective {
-  readonly forbiddenChars = input<(string | RegExp)[]>([]);
+  readonly inputRestrictions = input<InputRestrictions[]>([]);
   readonly invalidReservedWord = input<boolean | undefined>(false);
   private readonly ngControl = inject(NgControl);
 
   @HostListener('input')
   readonly onInput = (): void => {
-    let replaceValue: string = this.ngControl.control?.value ?? '';
-    for (const value of this.forbiddenChars()) {
-      replaceValue = replaceValue.replace(value, ''); // 入力可能な文字を制限
-      // .replace(/((?<=[+\-*/])[+\-*/])/g, '') // 符号の連続は禁止
-      // .replace(/(^|(?<=[^0-9]))0(?=[0-9])/g, ''); // 0始まりの数字は禁止
+    let retVal: string = this.ngControl.control?.value ?? '';
+    for (const { searchVal, replaceVal } of this.inputRestrictions()) {
+      retVal = retVal.replace(searchVal, replaceVal);
     }
 
     if (!this.invalidReservedWord()) {
       // 予約語が有効の場合
-      replaceValue = replaceValue.replace(Const.RESERVED_WORD, '');
+      retVal = retVal.replace(Const.RESERVED_WORD, '');
     }
 
-    this.ngControl.control?.setValue(replaceValue);
+    this.ngControl.control?.setValue(retVal);
   };
 }
